3541d85c316174cd6bf0d477623c2243998af18b
Slava Solodkov <dex252@bk.ru>
2020-04-17 04:07:46 +1000

    Complite Module 2

--- AspNetCore/Web.Restaurants/Pages/Restaurants/List.cshtml.cs
+++ AspNetCore/Web.Restaurants/Pages/Restaurants/List.cshtml.cs
@@ -2,24 +2,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data.Restaurants;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Models.Restaurants;
 
 namespace Web.Restaurants.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private readonly IRestaurant restaurantData;
         public IConfiguration config { get; set; }
         public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
 
-        public ListModel(IConfiguration config)
+        public ListModel(IConfiguration config, IRestaurant restaurantData)
         {
+            this.restaurantData = restaurantData;
             this.config = config;
         }
         public void OnGet()
         {
             Message = config["Message"];
+            Restaurants = restaurantData.GetAll();
         }
     }
 }
--- AspNetCore/Web.Restaurants/Startup.cs
+++ AspNetCore/Web.Restaurants/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data.Restaurants;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -24,6 +25,7 @@ namespace Web.Restaurants
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IRestaurant, InMemoryRestaurant>();
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
